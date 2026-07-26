669d95a095897ea0862671d0b0453abc84e0ed9d
DominikKos299 <dominikkosciolek05@gmail.com>
2026-07-26 02:50:59 -0400

    Wwise Project
     - music system creation
    - Generated Soundbanks
    - opened unity project to make sure everything is good
    - No console errors

---
+++ zero/zero_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,120 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace COMPLETEPUZZLEA
+        {
+            static const AkUniqueID GROUP = 3556769385U;
+
+            namespace STATE
+            {
+                static const AkUniqueID FALSE = 2452206122U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID TRUE = 3053630529U;
+            } // namespace STATE
+        } // namespace COMPLETEPUZZLEA
+
+        namespace COMPLETEPUZZLEB
+        {
+            static const AkUniqueID GROUP = 3556769386U;
+
+            namespace STATE
+            {
+                static const AkUniqueID FALSE = 2452206122U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID TRUE = 3053630529U;
+            } // namespace STATE
+        } // namespace COMPLETEPUZZLEB
+
+        namespace COMPLETEPUZZLEC
+        {
+            static const AkUniqueID GROUP = 3556769387U;
+
+            namespace STATE
+            {
+                static const AkUniqueID FALSE = 2452206122U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID TRUE = 3053630529U;
+            } // namespace STATE
+        } // namespace COMPLETEPUZZLEC
+
+        namespace COMPLETEPUZZLED
+        {
+            static const AkUniqueID GROUP = 3556769388U;
+
+            namespace STATE
+            {
+                static const AkUniqueID FALSE = 2452206122U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID TRUE = 3053630529U;
+            } // namespace STATE
+        } // namespace COMPLETEPUZZLED
+
+        namespace COMPLETEPUZZLEE
+        {
+            static const AkUniqueID GROUP = 3556769389U;
+
+            namespace STATE
+            {
+                static const AkUniqueID FALSE = 2452206122U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID TRUE = 3053630529U;
+            } // namespace STATE
+        } // namespace COMPLETEPUZZLEE
+
+        namespace GAMESTATE
+        {
+            static const AkUniqueID GROUP = 4091656514U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID PUZZLE = 1780448749U;
+                static const AkUniqueID TITLE = 3705726509U;
+            } // namespace STATE
+        } // namespace GAMESTATE
+
+        namespace PUZZLEVIEW
+        {
+            static const AkUniqueID GROUP = 2531350374U;
+
+            namespace STATE
+            {
+                static const AkUniqueID FALSE = 2452206122U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID TRUE = 3053630529U;
+            } // namespace STATE
+        } // namespace PUZZLEVIEW
+
+    } // namespace STATES
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MAIN_AUDIO_BUS = 2246998526U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
